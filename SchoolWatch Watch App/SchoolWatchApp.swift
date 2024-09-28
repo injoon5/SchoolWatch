ab1f8d88979a1d4a69bcb0f3f319a8f303a7ea08
Injoon Oh <injoon5@naver.com>
2024-09-28 14:59:08 +0900

    Initial Commit

---
+++ SchoolWatch Watch App/SchoolWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolWatchApp.swift
+//  SchoolWatch Watch App
+//
+//  Created by Injoon Oh on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SchoolWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
